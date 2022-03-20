3374d4875c70ec9eb197fd49f043b60f4bd1ae9e
LICHKING <savitarreus@gmail.com>
2022-03-20 13:37:30 +0200

    ❄️ BACKEND | First route ❄️

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,13 @@ const port = process.env.PORT || 5000;
 
 const app = express()
 
+app.get(
+	'/api/vehicles',
+	(req, res) => {
+		res.status(200).json({ result: 'Get vehicles', });
+	}
+)
+
 app.listen(
 	port, () => console.log(`Server started on port: ${port}`)
 );
